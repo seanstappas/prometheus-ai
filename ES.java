65459e4464ca68a62182ade938ff53ea0cbeadd4
Sean Stappas <seanstappas@gmail.com>
2017-01-27 21:42:32 -0500

    Skeleton of ES and KNN

---
+++ ES.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by Sean on 1/27/2017.
+ */
+public class ES {
+    HashMap<String, Rule> rules;
+    ArrayList<Fact> facts;
+    ArrayList<String> recommendations;
+
+    public ES() {
+        reset();
+    }
+
+    public void reset() {
+
+    }
+
+    public void addFact(String fact) {
+
+    }
+
+    public void addRule(Rule rule) {
+
+    }
+
+    private void addRecommendation(String recommendation) {
+
+    }
+
+    public ArrayList getRecommendation() {
+        return null;
+    }
+
+    public void think() {
+
+    }
+
+    class Rule {
+
+    }
+
+    class Fact {
+
+    }
+}
---
+++ KNN.java
@@ -0,0 +1,76 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by Sean on 1/27/2017.
+ */
+public class KNN {
+    HashMap<String, KN> mapKN;
+    ArrayList<KN> activeKN;
+    ArrayList<NN> activeNN;
+    ArrayList<String> activeMETA;
+
+    public KNN(String dbFilename) {
+
+    }
+
+    public void reset(String dbFilename) {
+
+    }
+
+    public void resetEmpty() {
+
+    }
+
+    public void saveKNN(String dbFilename) {
+
+    }
+
+    public void addNN(NN nn) {
+
+    }
+
+    public void clearNN() {
+
+    }
+
+    public void clearMETA() {
+
+    }
+
+    public void clearKN() {
+
+    }
+
+    public void newKN(KN kn) {
+
+    }
+
+    public void delKN(String hashTag) {
+
+    }
+
+    public ArrayList think() {
+        return null;
+    }
+
+    private void thinkBackwards() {
+
+    }
+
+    private void thinkLambda() {
+
+    }
+
+    private void thinkForwards() {
+
+    }
+
+    class KN {
+
+    }
+
+    class NN {
+
+    }
+}
