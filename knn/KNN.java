9acfe9bae1a1f7b80089345d4f55ab335a644e6f
Sean Stappas <seanstappas1@web103252.wireless.mcgill.ca>
2017-03-10 15:33:42 -0500

    Added answers to questions

--- knn/KNN.java
+++ knn/KNN.java
@@ -7,19 +7,19 @@ import java.util.*;
  */
 public class KNN {
 
-    // TODO: Once again changed data structures to sets (Is order important?)
+    // TODO: Once again changed data structures to sets (Is order important?) Good.
     // Spec data structures
     private HashMap<String, KN> mapKN; // Key here is the "hashTag" field of KN
-    private Set<KN> activeKN; // TODO: Are these the KNs whose activation > threshold? What do we do with this?
-    private Set<NN> activeNN; // TODO: How do these NN tuples translate to the String KN tags? What do we do with this?
-    private Set<String> activeMETA; // TODO: Are these the "true" (active) tags in the system? Or is this just a set of META layers? If so, how do we keep track of activated tags? Should there be a list of "facts"?
+    private Set<KN> activeKN; // TODO: Are these the KNs whose activation > threshold? What do we do with this? Fired KNs (to check if need to fire others)
+    private Set<NN> activeNN; // TODO: How do these NN tuples translate to the String KN tags? What do we do with this? (Will need some interface to translate int + string to string)
+    private Set<String> activeMETA; // Commands coming from meta
 
 
     // Added structures
-    int numberOfCycles; // Number of times the network should update its state and propogate. For now, just go to quiescence. TODO: Is this a field? How will this be set?
-    private Set<String> facts; // "Facts", activated tags stored here TODO: Is this the right way to store facts?
+    int numberOfCycles; // Number of times the network should update its state and propogate. For now, just go to quiescence.
+    private Set<String> facts; // "Facts", activated tags stored here TODO: Is this the right way to store facts? Yes
 
-    public KNN(String dbFilename) { // TODO: What kind of database are we using?
+    public KNN(String dbFilename) { // TODO: What kind of database are we using? Probably CSV for now
         mapKN = new HashMap<>();
         activeKN = new HashSet<>();
         activeNN = new HashSet<>();
@@ -78,10 +78,11 @@ public class KNN {
      *
      * @return
      */
-    // TODO: How does this method choose the proper think method?
-    // TODO: What does think() return?
+    // TODO: How does this method choose the proper think method? For now, it will decide based on command from META. (activeMETA)
+    // TODO: What does think() return? ...
     public ArrayList think() {
-        thinkForwards();
+        thinkForwards(); // What we want in the future: If no RECOMMENDATIONS fired = thinkForwards failed, resort to either thinkBackwards or thinkLambda (Not theoretically well understood)
+        // People generally thinkBackwards all the time in the background. In the future, could have a background thread that thinks backwards...
         return null;
     }
 
@@ -120,7 +121,7 @@ public class KNN {
     /**
      * Simple forward activation of knowledge nodes. If input tag activated and activation > threshold: output tags are activated, and this is cascaded through the network.
      */
-    private void thinkForwards() { // TODO: How is a cycle defined? Activate each key in facts? (For now: just go to quiescence)
+    private void thinkForwards() { // TODO: How is a cycle defined? Just activate current tags in Facts, no more
         Set<String> pendingFacts = new HashSet<>();
         do {
             pendingFacts.clear();
@@ -156,15 +157,15 @@ public class KNN {
     class KN {
         String hashTag;
         int activation = 0; // int starts at 0 goes to 1 (can be sigmoid, or jump to 1). Increases when sees tag.
-        // TODO: Is threshold the same for all nodes?
+        // TODO: Is threshold the same for all nodes? No
         int threshold = 1; // limit: When activation > threshold : fires output tags (strings array). These tags can be lists of rules or facts.
         int age = 0; // When a node is newly formed it has an age of zero.
         // When the node’s age increases to a value greater than or equal to K the node is then deleted.
         // The age parameter ages in a particular way.  It ages only if it is not used.  Every time a node is used the age is reset to zero.
         // If the node is not used after an “tau” amount of time it will age.
         // Ages linearly or using sigmoid.
-        int strength; // TODO: How is strength used?
-        int confidence; // TODO: How is confidence used?
+        int strength; // TODO: How is strength used? Read doc...
+        int confidence; // TODO: How is confidence used? Read doc...
         String[] strings; // What is this for? These are the output tags, fired when activation > threshold.
 
         public KN(String hashTag, String[] strings) {
@@ -192,7 +193,7 @@ public class KNN {
 
         }
 
-        // TODO: Should age increment at every think() cycle? Or independently, after some amount of time?
+        // TODO: Should age increment at every think() cycle? Or independently, after some amount of time? System will have (daily) timestamp, nodes will have timestamp updated at every firing. Look at difference between the two before deciding to fire
         public void age() {
             age++;
         }
