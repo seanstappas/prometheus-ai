c5136969d8829788dfa09b27f3dd250b9ad75209
Sean Stappas <seanstappas@gmail.com>
2017-11-12 10:37:18 -0500

    Testing invalid tests.

--- src/test/java/integration/TestKNNandES.java
+++ src/test/java/integration/TestKNNandES.java
@@ -17,6 +17,7 @@ import java.io.FileReader;
 import java.util.*;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 /**
  * Running es.internal.NeuralNetworkImpl and knn.internal.KnowledgeNodeNetwork
@@ -215,6 +216,8 @@ public class TestKNNandES {
     @Test
     public void lambdaSearchTest() {
         System.out.println("***Lambda Search Test***");
+
+        assertTrue(false);
 
         ArrayList<Tuple> inputs = new ArrayList<>();
         Tuple data1 = new Tuple("mammal", 10);
