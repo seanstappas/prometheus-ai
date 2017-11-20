c383a5b094d86dcc3b25f1fa3a79a96b6c7c6196
Sean Stappas <seanstappas@gmail.com>
2017-11-20 00:01:59 -0500

    Beautified graphing, with colors and clicking functionality.

--- src/test/java/graphing/KnnGrapher.java
+++ src/test/java/graphing/KnnGrapher.java
@@ -7,16 +7,28 @@ import knn.api.KnowledgeNodeNetwork;
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.Node;
 import org.graphstream.graph.implementations.SingleGraph;
+import org.graphstream.ui.view.Viewer;
+import org.graphstream.ui.view.ViewerListener;
+import org.graphstream.ui.view.ViewerPipe;
 import prometheus.api.Prometheus;
 import prometheus.guice.PrometheusModule;
 import tags.Tag;
 
 import java.util.*;
 
-public class KnnGrapher {
+public class KnnGrapher implements ViewerListener {
+    Graph graph;
+    Set<String> activeIDs;
+
     private static final String ANIMAL_DATA_PATH = "data/animalData.txt";
 
     public static void main(String args[]) throws InterruptedException {
+        new KnnGrapher();
+    }
+
+    public KnnGrapher() throws InterruptedException {
+        System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
+
         // Create KNN
         Prometheus prometheus = Guice.createInjector(new PrometheusModule()).getInstance(Prometheus.class);
         KnowledgeNodeNetwork knn = prometheus.getKnowledgeNodeNetwork();
@@ -25,7 +37,7 @@ public class KnnGrapher {
 
         // Activate random Tags
         Collections.shuffle(knowledgeNodes);
-        Set<String> activeIDs = new HashSet<>();
+        activeIDs = new HashSet<>();
         for (int i = 0; i < knowledgeNodes.size() / 2; i++) {
             Tag inputTag = knowledgeNodes.get(i).getInputTag();
             knn.addActiveTag(inputTag);
@@ -33,38 +45,64 @@ public class KnnGrapher {
         }
 
 
-        Graph graph = new SingleGraph("tutorial 1");
-
-        String styleSheet = "node {" +
-                "	fill-color: black;" +
-                "}" +
-                "node.marked {" +
-                "	fill-color: red;" +
-                "}";
-        graph.addAttribute("ui.stylesheet", styleSheet);
+        graph = new SingleGraph("tutorial 1");
+        graph.addAttribute("ui.stylesheet", "url('data/graph_style_sheet.txt')");
         graph.setAutoCreate(true);
-        graph.setStrict(false);
-        graph.display();
+        graph.setStrict(false);graph.addAttribute("ui.quality");
+        graph.addAttribute("ui.antialias");
+        Viewer viewer = graph.display();
+
+        ViewerPipe fromViewer = viewer.newViewerPipe();
+        fromViewer.addViewerListener(this);
+        fromViewer.addSink(graph);
 
         for (KnowledgeNode knowledgeNode : knowledgeNodes) {
             String inputID = knowledgeNode.getInputTag().toString();
             for (Tag t : knowledgeNode.getOutputTags()) {
                 String outputID = t.toString();
                 graph.addEdge(inputID + outputID, inputID, outputID, true);
-                updateLabels(activeIDs, graph);
+                updateLabels();
                 Thread.sleep(100);
             }
         }
 
+        while (true) fromViewer.pump();
+
     }
 
-    private static void updateLabels(Set<String> activeIDs, Graph graph) {
+    private void updateLabels() {
         for (Node node : graph) {
-            String id = node.getId();
-            node.addAttribute("ui.label", id);
-            if (activeIDs.contains(id)) {
-                node.setAttribute("ui.class", "marked");
-            }
+            updateNode(node);
         }
+    }
+
+    private void updateNode(Node node) {
+        String id = node.getId();
+        String nodeType = id.split("\\[")[0].toLowerCase();
+        node.addAttribute("ui.label", id);
+        if (activeIDs.contains(id)) {
+            node.setAttribute("ui.class", "marked");
+        } else {
+            node.setAttribute("ui.class", nodeType);
+        }
+    }
+
+    @Override
+    public void viewClosed(String viewName) {
+
+    }
+
+    @Override
+    public void buttonPushed(String id) {
+        System.out.println("Button pushed on node " + id);
+        Node node = graph.getNode(id);
+        node.setAttribute("ui.class", "clicked");
+    }
+
+    @Override
+    public void buttonReleased(String id) {
+        System.out.println("Button released on node "+id);
+        Node node = graph.getNode(id);
+        updateNode(node);
     }
 }
