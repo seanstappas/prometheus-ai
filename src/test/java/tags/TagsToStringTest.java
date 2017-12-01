346eca2b1139764d1fe26044edf6ecb8f6f9b116
Sean Stappas <seanstappas@gmail.com>
2017-11-30 21:45:27 -0500

    Created "simple" ES test.

--- src/test/java/tags/TagsToStringTest.java
+++ src/test/java/tags/TagsToStringTest.java
@@ -12,7 +12,17 @@ public class TagsToStringTest {
         Tag t2 = new Recommendation("P(B)");
         System.out.println(t2.simpleToString());
 
-        Tag t3 = new Rule("P(A) -> @P(B)");
+        Rule t3 = new Rule("P -> @P");
+        System.out.println(t3.getInputFacts());
+        for (Fact f : t3.getInputFacts()) {
+            System.out.println(f.getPredicateName());
+            System.out.println(f.getArguments());
+        }
+        System.out.println(t3.getOutputPredicates());
+        for (Predicate p : t3.getOutputPredicates()) {
+            System.out.println(p.getPredicateName());
+            System.out.println(p.getArguments());
+        }
         System.out.println(t3.simpleToString());
     }
 }
